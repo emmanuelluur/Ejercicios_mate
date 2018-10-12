c016fae5bbf7c8c9c493d9b46d00fa08624f93a4
Emmanuel Lucio Urbina <emmanuelluur@gmail.com>
2018-10-11 23:35:48 -0500

    + metodo post js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,46 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     //  barra navegacion
-    var elems = document.querySelectorAll('.sidenav')
-    var instances = M.Sidenav.init(elems)
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
     //  selects
-    var elems = document.querySelectorAll('select')
-    var instances = M.FormSelect.init(elems)
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
     //  rango de numeros
 });
 
-function Generar(){
+function Generar() {
     //  obtenemos el boton
-    let btn = document.querySelector("#genera")
+    let btn = document.querySelector("#genera");
     //  agregamos el evento click 
-    btn.addEventListener("click", function(){
+    btn.addEventListener("click", function () {
         //  obtenemos infor
-        let op = document.querySelector("#op")
-        let tipo = document.querySelector("#tipo")
-        let ini = document.querySelector("#ini")
-        let fin = document.querySelector("#fin")
-        let datos = [op.value, tipo.value, ini.value, fin.value]
-            
-        
-        console.log(datos);
-        
+        let op = document.querySelector("#op");
+        let tipo = document.querySelector("#tipo");
+        let ini = document.querySelector("#ini");
+        let fin = document.querySelector("#fin");
+        let url = "source/generar.php";  //  URL que recibira y retornara las peticiones
+        let request; // variable que se encargara de las peticiones
+        //  peticion a servidor
+        request = new XMLHttpRequest();  //  creamos nueva request http 
+        request.open('POST', url, true);    //  abrimos peticion POST
+        request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');  //  Cabeceras 
+        request.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                // si se encuentra el archivo y esta todo OK mostramos resultado
+                console.log(this.responseText);
+                
+            }
+        }
+        request.send(
+            "operacion=" + op.value +
+            "&tipo=" + tipo.value +
+            "&inicio=" + ini.value +
+            "&fin=" + fin.value +
+            "&generar_ope=generar_ope");
     });
+
+
+
 }
 
